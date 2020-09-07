6f36bc97e1a54954fb392cc16f2c2b09c6946ef2
meteatbas <meteatbas154@gmail.com>
2020-09-07 22:39:53 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -7,29 +7,55 @@ import TodoList from './components/TodoList'
 
 class App extends Component {
   state={
-    items:[{id:1,title:'wake up'},{id:2,title:'telephone'}],
+    items:[],
     id:uuid(),
     item:'',
     editItem:false
   }
   handleChange=(e)=>{
-    console.log('handle change');
+    this.setState({
+      item:e.target.value
+    })
   }
   handleSubmit=(e)=>{
-    console.log('handle Submit');
+    e.preventDefault();
+    const newItem={
+      id:this.state.id,
+      title:this.state.item
+    }
+    const updatedItems=[...this.state.items,newItem]
+    this.setState({
+      items:updatedItems,
+      item:'',
+      id:uuid(),
+      editItem:false
+    })
   }
   clearList=(id)=>{
-    console.log('Clear List');
+    this.setState({
+      items:[]
+    })
   }
   handleDelete=(id)=>{
-    console.log(`handle edit ${id}`);
+    const filteredItems=this.state.items.filter(item=>item.id!==id)
+    this.setState({
+      items:filteredItems
+    })
   }
   handleEdit=(id)=>{
-    console.log(`edit edit ${id}`);
+    const filteredItems=this.state.items.filter(item=>item.id!==id)
+    const selectedItem=this.state.items.find(item=>item.id===id);
+    this.setState({
+      items:filteredItems,
+      item:selectedItem.title,
+      id:id,
+      editItem:true
+    })
   }
   render(){
     // console.log(this.state);
     return (
+
     <div className="container">
       <div className="row">
         <div className="col-10 mx-auto col-md-8 mt-5">
